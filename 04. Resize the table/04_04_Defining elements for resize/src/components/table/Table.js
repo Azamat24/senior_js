fd5023cffcce50fe7b397967b563b377839b07d2
root <root@anb>
2020-10-25 19:41:59 +0600

    04. Resize the table

---
+++ 04. Resize the table/04_04_Defining elements for resize/src/components/table/Table.js
@@ -0,0 +1,22 @@
+import {ExcelComponent} from '@core/ExcelComponent'
+import {createTable} from '@/components/table/table.template'
+
+export class Table extends ExcelComponent {
+  static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
+  toHTML() {
+    return createTable(20)
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      console.log('Start resizing', event.target.dataset.resize)
+    }
+  }
+}
---
+++ 04. Resize the table/04_10_ Optimization + Performance/src/components/table/Table.js
@@ -0,0 +1,47 @@
+import {ExcelComponent} from '@core/ExcelComponent'
+import {createTable} from '@/components/table/table.template'
+import {$} from '@core/dom'
+
+export class Table extends ExcelComponent {
+  static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
+  toHTML() {
+    return createTable(20)
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target)
+      // const $parent = $resizer.$el.parentNode // bad!
+      // const $parent = $resizer.$el.closest('.column') // better but bad
+      const $parent = $resizer.closest('[data-type="resizable"]')
+      const coords = $parent.getCoords()
+
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+
+      document.onmousemove = e => {
+        console.log('mousemove')
+        const delta = e.pageX - coords.right
+        const value = coords.width + delta
+        $parent.$el.style.width = value + 'px'
+        cells.forEach(el => el.style.width = value + 'px')
+      }
+
+      document.onmouseup = () => {
+        document.onmousemove = null
+      }
+    }
+  }
+}
+
+// 589 msScripting
+// 2433 msRendering
+
+// 440 msScripting
+// 1771 msRendering
